7eaea8ee8527b85138a46a7dd3dde374ba51a123
Khaldoun TAKTAK <khaldoun.taktak@telecom-sudparis.eu>
2024-09-29 23:06:03 +0200

    finished controller part

--- src/Controller/AlbumController.php
+++ src/Controller/AlbumController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Album;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\AlbumRepository;
+use Doctrine\Persistence\ManagerRegistry;
 
 
 class AlbumController extends AbstractController
@@ -21,7 +23,7 @@ class AlbumController extends AbstractController
       $albums= $albumRepository->findAll();
 
       foreach($albums as $album){
-        $htmlpage .= '<li>' . $album->getName() . '</li>';
+        $htmlpage .= '<li> <a href="' . $this->generateUrl('album_show', ['id' => $album->getId()]) . '">' . $album->getName() . ' </a> </li>';
       }
       
 
@@ -40,5 +42,28 @@ class AlbumController extends AbstractController
             );
     }
 
-    
+  /**
+ * Show a album
+ *
+ * @param Integer $id (note that the id must be an integer)
+ */
+#[Route('/album/{id}', name: 'album_show', requirements: ['id' => '\d+'])]
+public function show(ManagerRegistry $doctrine, $id) : Response
+{
+        $albumRepo = $doctrine->getRepository(Album::class);
+        $album = $albumRepo->find($id);
+
+        if (!$album) {
+                throw $this->createNotFoundException('The album does not exist');
+        }
+
+        $res = 'Le nom de cet album est ' . $album->getName();
+        //...
+
+        $res .= '<p/><a href="' . $this->generateUrl('app_album') . '">Back</a>';
+
+        return new Response('<html><body>'. $res . '</body></html>');
+}
+
+
 }
